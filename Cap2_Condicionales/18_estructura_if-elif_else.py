39bccecfad14119fcfa770ef4db4b10c9e5221e5
Rafael <rvelascobalazs@outlook.cl>
2021-11-14 20:36:30 -0300

    Inicia 2do Capitulo, condicional if-elif-else

---
+++ Cap2_Condicionales/18_estructura_if-elif_else.py
@@ -0,0 +1,10 @@
+#condicionales
+
+numero = int(input("Ingrese un numero: "))
+
+if numero > 0 :
+    print("El número es positivo")
+elif numero < 0 :
+    print("El número es negativo")
+else :
+    print("El número es 0")
